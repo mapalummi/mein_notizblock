36a9bd1dce97b78b2d9b8ca62e559c0a0d8a5cbb
marco <mapalummi@outlook.de>
2025-03-11 17:32:19 +0100

    update 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let myTitles = ['Testing', 'Exercise', 'What'];
-let myNotes = ['Test', 'Sets', 'this'];
+let myTitles = ["Testing", "Exercise", "What"];
+let myNotes = ["Test", "Sets", "This"];
 
 let archiveNotesTitles = [];
 let archiveNotes = [];
@@ -7,149 +7,165 @@ let archiveNotes = [];
 let trashNotesTitles = [];
 let trashNotes = [];
 
+//Achtung neu!
+function init(){
+    noteFromLocalStorage();
+    renderNotes();
+}
 
 
 //Funktioniert:
-function renderNotes(){
-    document.getElementById('note_content').innerHTML = "";
+function renderNotes() {
+  document.getElementById("note_content").innerHTML = "";
 
-    for (let i = 0; i < myNotes.length; i++) {
-        document.getElementById('note_content').innerHTML += getNoteTemplate(i);
+  for (let i = 0; i < myNotes.length; i++) {
+    document.getElementById("note_content").innerHTML += getNoteTemplate(i);
+  }
+}
+
+function renderArchive() {
+  document.getElementById("archive_content").innerHTML = "";
+
+  for (let iArchive = 0; iArchive < archiveNotes.length; iArchive++) {
+    document.getElementById("archive_content").innerHTML +=
+      getArchiveTemplate(iArchive);
+  }
+}
+
+//Funktioniert:
+function renderTrashNotes() {
+  document.getElementById("trash_content").innerHTML = "";
+
+  for (let iTrash = 0; iTrash < trashNotes.length; iTrash++) {
+    document.getElementById("trash_content").innerHTML +=
+      getTrashNoteTemplate(iTrash);
+  }
+}
+
+//Funktioniert:
+function addNote() {
+  let inputTitleValue = document.getElementById("input_title").value;
+  let inputTextValue = document.getElementById("input_text").value;
+
+  if (inputTitleValue == "") {
+    alert("Titel fehlt");
+  } else if (inputTextValue == "") {
+    alert("Text fehlt");
+  } else {
+    myTitles.push(inputTitleValue);
+    myNotes.push(inputTextValue);
+
+    //Achtung neu!
+    noteToLocalStorage();
+
+    renderNotes();
+
+    document.getElementById("input_title").value = "";
+    document.getElementById("input_text").value = "";
+  }
+}
+
+
+//Achtung neu:
+function noteToLocalStorage(){
+    localStorage.setItem("myTitles", JSON.stringify(myTitles));
+    localStorage.setItem("myNotes", JSON.stringify(myNotes));
+}
+
+function noteFromLocalStorage(){
+    let newTitles = JSON.parse(localStorage.getItem("myTitles"));
+    let newNotes = JSON.parse(localStorage.getItem("myNotes"));
+
+    if (newTitles != null && newNotes != null) {
+        myTitles = newTitles;
+        myNotes = newNotes;
     }
 }
 
 
 
 
-function renderArchive(){
-    document.getElementById('archive_content').innerHTML = "";
-
-    for (let iArchive = 0; iArchive < archiveNotes.length; iArchive++) {
-        document.getElementById('archive_content').innerHTML += getArchiveTemplate(iArchive);
-    }
-}
-
-
 
 
 //Funktioniert:
-function renderTrashNotes(){
-    document.getElementById('trash_content').innerHTML = "";
+function noteToTrash(i) {
+  let trashNote = myNotes.splice(i, 1);
+  trashNotes.push(trashNote[0]);
 
-    for (let iTrash = 0; iTrash < trashNotes.length; iTrash++) {
-        document.getElementById('trash_content').innerHTML += getTrashNoteTemplate(iTrash);
-    }
+  let trashNoteTitle = myTitles.splice(i, 1);
+  trashNotesTitles.push(trashNoteTitle[0]);
+
+  renderNotes();
+  renderArchive();
+  renderTrashNotes();
 }
 
+function noteToArchive(i) {
+  let archiveNote = myNotes.splice(i, 1);
+  archiveNotes.push(archiveNote[0]);
+
+  let archiveNoteTitle = myTitles.splice(i, 1);
+  archiveNotesTitles.push(archiveNoteTitle[0]);
+
+  renderNotes();
+  renderArchive();
+  renderTrashNotes();
+}
 
 //Funktioniert:
-function addNote(){
-    let inputTitleValue = document.getElementById('input_title').value;
-    let inputTextValue = document.getElementById('input_text').value;
+function archiveNoteToTrash(iArchive) {
+  let archiveNote = archiveNotes.splice(iArchive, 1);
+  trashNotes.push(archiveNote[0]);
 
-    if (inputTitleValue == "") {
-        alert("Titel fehlt")
-    } else if (inputTextValue == "") {
-        alert("Text fehlt")
-    } 
-    else{
+  let archiveNoteTitle = archiveNotesTitles.splice(iArchive, 1);
+  trashNotesTitles.push(archiveNoteTitle[0]);
 
-        myTitles.push(inputTitleValue);
-        myNotes.push(inputTextValue);
-    
-        renderNotes();
-    
-        document.getElementById('input_title').value = "";
-        document.getElementById('input_text').value = "";
-
-    }
-
+  renderNotes();
+  renderArchive();
+  renderTrashNotes();
 }
-
-
-
-
-
 
 //Funktioniert:
-function noteToTrash(i){
-    let trashNote = myNotes.splice(i, 1);
-    trashNotes.push(trashNote[0]);
+function archiveNoteRestore(iArchive) {
+  let restoreArchiveNote = archiveNotes.splice(iArchive, 1);
+  myNotes.push(restoreArchiveNote[0]);
 
-    let trashNoteTitle = myTitles.splice(i, 1);
-    trashNotesTitles.push(trashNoteTitle[0]);
+  let restoreArchiveTitle = archiveNotesTitles.splice(iArchive, 1);
+  myTitles.push(restoreArchiveTitle[0]);
 
-    renderNotes();
-    renderArchive();
-    renderTrashNotes();
+  renderNotes();
+  renderArchive();
+  renderTrashNotes();
 }
 
+//Funktioniert:
+function deletedNoteRestore(iTrash) {
+  let restoreNote = trashNotes.splice(iTrash, 1);
+  archiveNotes.push(restoreNote[0]);
 
+  let restoreTitle = trashNotesTitles.splice(iTrash, 1);
+  archiveNotesTitles.push(restoreTitle[0]);
 
-function noteToArchive(i){
-    let archiveNote = myNotes.splice(i, 1);
-    archiveNotes.push(archiveNote[0]);
-
-    let archiveNoteTitle = myTitles.splice(i, 1);
-    archiveNotesTitles.push(archiveNoteTitle[0]);
-
-    renderNotes();
-    renderArchive();
-    renderTrashNotes();
+  renderNotes();
+  renderArchive();
+  renderTrashNotes();
 }
-
-
-
-//Anpassen!!!
-function archiveNoteToTrash(iArchive){
-    let archiveNote = archiveNotes.splice(iArchive, 1);
-    trashNotes.push(archiveNote[0]);
-
-    let archiveNoteTitle = archiveNotesTitles.splice(iArchive, 1);
-    trashNotesTitles.push(archiveNoteTitle[0]);
-
-    renderNotes();
-    renderArchive();
-    renderTrashNotes();
-}
-
-
-
-// Anpassen!!
-function archiveNoteRestore(iArchive){
-    let restoreArchiveNote = archiveNotes.splice(iArchive, 1);
-    myNotes.push(restoreArchiveNote[0]);
-
-    let restoreArchiveTitle = archiveNotesTitles.splice(iArchive, 1);
-    myTitles.push(restoreArchiveTitle[0]);
-
-    renderNotes();
-    renderArchive();
-    renderTrashNotes();
-}
-
-
-
-//Anpassen!!!!
-function deletedNoteRestore(iTrash){
-    let restoreNote = trashNotes.splice(iTrash, 1);
-    archiveNotes.push(restoreNote[0]);
-
-    let restoreTitle = trashNotesTitles.splice(iTrash, 1);
-    archiveNotesTitles.push(restoreTitle[0]);
-
-    renderNotes();
-    renderArchive();
-    renderTrashNotes();
-}
-
 
 //Funktioniert:
 function deleteNote(iTrash) {
-    trashNotes.splice(iTrash, 1);
-    trashNotesTitles.splice(iTrash, 1);
+  trashNotes.splice(iTrash, 1);
+  trashNotesTitles.splice(iTrash, 1);
 
-    renderNotes();
-    renderArchive();
-    renderTrashNotes();
+  renderNotes();
+  renderArchive();
+  renderTrashNotes();
+}
+
+
+function addOverlay(){
+  document.getElementById('overlay').classList.remove('d_none');
+}
+
+function removeOverlay(){
+  document.getElementById('overlay').classList.add('d_none');
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,16 +1,31 @@
 //Funktioniert:
-function getNoteTemplate(i){
-    return `<div class="note"><p><b>${myTitles[i]}</b><br><i>${myNotes[i]}</p><a onclick="noteToTrash(${i})" href="#">Papierkorb</a><a onclick="noteToArchive(${i})" href="#">archivate</a></div>`;
+function getNoteTemplate(i) {
+  return `<div class="note">
+            <p><b>${myTitles[i]}</b><br><i>${myNotes[i]}</p>
+            <div class="btn_icons">
+            <a onclick="noteToTrash(${i})" href="#"><img src="./assets/icons/icons8-müll-löschen.png" alt=""></a>
+            <a onclick="noteToArchive(${i})" href="#"><img src="./assets/icons/icons8-lesezeichen.png" alt=""></a>
+            </div>
+          </div>`;
 }
 
-
-
-function getArchiveTemplate(iArchive){
-    return `<div class="archive_note"><p><b>${archiveNotesTitles[iArchive]}</b><br><i>${archiveNotes[iArchive]}</p><a onclick="archiveNoteToTrash(${iArchive})" href="#">Papierkorb</a><a onclick="archiveNoteRestore(${iArchive})" href="#">restore</a></div>`;
+function getArchiveTemplate(iArchive) {
+  return `<div class="archive_note">
+            <p><b>${archiveNotesTitles[iArchive]}</b><br><i>${archiveNotes[iArchive]}</p>
+            <div class="btn_icons">
+            <a onclick="archiveNoteToTrash(${iArchive})" href="#"><img src="./assets/icons/icons8-müll-löschen.png" alt=""></a>
+            <a onclick="archiveNoteRestore(${iArchive})" href="#"><img src="assets/icons/icons8-lesezeichen-löschen.png" alt=""></a>
+            </div>
+          </div>`;
 }
-
 
 //Funktioniert:
-function getTrashNoteTemplate(iTrash){
-    return `<div class="del_note"><p>${trashNotesTitles[iTrash]}<br>${trashNotes[iTrash]}</p><a onclick="deleteNote(${iTrash})" href="#">Delete</a><a onclick="deletedNoteRestore(${iTrash})" href="#">archivate</a></div>`;
+function getTrashNoteTemplate(iTrash) {
+  return `<div class="del_note">
+            <p><b>${trashNotesTitles[iTrash]}</b><br><br>${trashNotes[iTrash]}</p>
+            <div class="btn_icons">
+            <a onclick="deleteNote(${iTrash})" href="#"><img src="./assets/icons/icons8-unwiederuflich-löschen.png" alt=""></a>
+            <a onclick="deletedNoteRestore(${iTrash})" href="#"><img src="./assets/icons/icons8-lesezeichen-hinzufügen.png" alt=""></a>
+            </div>
+          </div>`;
 }
